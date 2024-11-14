27d3adfee43a0568231b7bc4500d5f60ab19ee17
Saymon100 <169170374+Saymon100@users.noreply.github.com>
2024-11-14 11:39:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Mustang',
+    'Que data a mustang foi fundada?',
+    'Em 17 de abril de 1964.Foi estreado o primeiro carro ford mustang em nova york'
+)
+
+criaCartao(
+    'Simbolo',
+    'Qual o simbolo da mustang?',
+    'Um cavalo correndo.Tendo siginificado Determinação e Persistência'
+)
+
+criaCartao(
+    'Mustang',
+    'O que significa a palavra "Mustang"?',
+    'Mustang pode referir-se a: Mustangue - raça de cavalo selvagem que vive nas planícies do oeste e sudeste norte americano'
+)
+
+criaCartao(
+    'Mustang',
+    'Onde fica a tração do Mustang??',
+    ' tração traseira e o importante motor V8, no entanto, o modelo ganhará mais opções mecânicas para atender a consumidores diferentes nos próximos anos.'
+)
